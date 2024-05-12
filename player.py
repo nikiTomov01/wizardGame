4a11f7c87a1fbcf5a56835f8f2e8c48713814f82
Nik <nikolaitomov25@gmail.com>
2024-05-12 12:16:27 +0300

    added player image and updated player.draw() to blit image

--- player.py
+++ player.py
@@ -4,8 +4,9 @@ from settings import *
 class Player:
     def __init__(self, game):
         self.game = game
-        self.x = 50
-        self.y = 50
+        self.x = 800 / 2
+        self.y = 600 / 2
+        self.player_image = pygame.image.load("./character/pixelFlameChar.png").convert_alpha()
         
     def movement(self):
         speed = SPEED * self.game.delta_time
@@ -21,7 +22,10 @@ class Player:
             self.x -= speed
 
     def draw(self):
-        pygame.draw.rect(self.game.screen, (0, 0, 0), [self.x, self.y, 32, 32])
+        #pygame.draw.rect(self.game.screen, (0, 0, 0), [self.x, self.y, 32, 32])
+        player_rect = self.player_image.get_rect(center = (self.x, self.y))
+        self.game.screen.blit(self.player_image, player_rect)
+
 
     def update(self):
         self.movement()
