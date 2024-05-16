6078b06ff1e189a1624ddc279394abcece01a85c
Nik <nikolaitomov25@gmail.com>
2024-05-16 16:15:31 +0300

    added ui card class which draws a ui card on the screen and returns True if the card is clicked with the left mouse button

---
+++ uiCard.py
@@ -0,0 +1,34 @@
+import pygame
+
+class UICard:
+    def __init__(self, game, x, y, image):
+        self.game = game
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width), int(height)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+    
+    def draw(self):
+        action = False
+
+        #get mouse position 
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        # draw card on screen
+        self.game.screen.blit(self.image, self.rect)
+
+        if action == True:
+            del self
+
+        return action
+        
