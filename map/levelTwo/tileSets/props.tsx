b1e4eeff21f42cbfd8cbf5657166c08642ebebe9
Nik <nikolaitomov25@gmail.com>
2024-05-21 13:08:21 +0300

    updated levelTwo tile sets to get tiles from relative folder for project and not local folder from pc

--- map/levelTwo/tileSets/props.tsx
+++ map/levelTwo/tileSets/props.tsx
@@ -2,15 +2,15 @@
 <tileset version="1.10" tiledversion="1.10.2" name="props" tilewidth="66" tileheight="77" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="66" height="77" source="../../../../../../game assets/fields/2 Objects/7 Decor/Tree1.png"/>
+  <image width="66" height="77" source="../../../mapStuff/tileSetImages/props/tree1.png"/>
  </tile>
  <tile id="1">
-  <image width="27" height="21" source="../../../../../../game assets/fields/2 Objects/4 Stone/10.png"/>
+  <image width="27" height="21" source="../../../mapStuff/tileSetImages/props/11.png"/>
  </tile>
  <tile id="2">
-  <image width="35" height="30" source="../../../../../../game assets/fields/2 Objects/4 Stone/11.png"/>
+  <image width="35" height="30" source="../../../mapStuff/tileSetImages/props/10.png"/>
  </tile>
  <tile id="3">
-  <image width="37" height="27" source="../../../../../../game assets/fields/2 Objects/4 Stone/7.png"/>
+  <image width="37" height="27" source="../../../mapStuff/tileSetImages/props/7.png"/>
  </tile>
 </tileset>
