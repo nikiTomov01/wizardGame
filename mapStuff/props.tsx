23563eed3b7449834e16a69d4d8891d8e019bdd9
Nik <nikolaitomov25@gmail.com>
2024-05-21 12:23:49 +0300

    updated map.tsx to use files from a relative directory instead of from a directory on the pc on which i started the project.

---
+++ mapStuff/props.tsx
@@ -0,0 +1,163 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="props" tilewidth="66" tileheight="77" tilecount="53" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="63">
+  <image width="35" height="30" source="E:/game assets/fields/2 Objects/4 Stone/11.png"/>
+ </tile>
+ <tile id="64">
+  <image width="27" height="21" source="E:/game assets/fields/2 Objects/4 Stone/10.png"/>
+ </tile>
+ <tile id="66">
+  <image width="38" height="23" source="E:/game assets/fields/2 Objects/4 Stone/8.png"/>
+ </tile>
+ <tile id="65">
+  <image width="37" height="27" source="E:/game assets/fields/2 Objects/4 Stone/7.png"/>
+ </tile>
+ <tile id="67">
+  <image width="66" height="77" source="E:/game assets/fields/2 Objects/7 Decor/Tree1.png"/>
+ </tile>
+ <tile id="68">
+  <image width="34" height="21" source="E:/game assets/fields/2 Objects/7 Decor/Log1.png"/>
+ </tile>
+ <tile id="69">
+  <image width="33" height="32" source="E:/game assets/fields/2 Objects/7 Decor/Log2.png"/>
+ </tile>
+ <tile id="70">
+  <image width="45" height="14" source="E:/game assets/fields/2 Objects/7 Decor/Log3.png"/>
+ </tile>
+ <tile id="71">
+  <image width="11" height="33" source="E:/game assets/fields/2 Objects/7 Decor/Log4.png"/>
+ </tile>
+ <tile id="72">
+  <image width="57" height="36" source="E:/game assets/fields/2 Objects/8 Camp/1.png"/>
+ </tile>
+ <tile id="73">
+  <image width="36" height="51" source="E:/game assets/fields/2 Objects/8 Camp/2.png"/>
+ </tile>
+ <tile id="74">
+  <image width="53" height="34" source="E:/game assets/fields/2 Objects/8 Camp/3.png"/>
+ </tile>
+ <tile id="75">
+  <image width="56" height="38" source="E:/game assets/fields/2 Objects/8 Camp/4.png"/>
+ </tile>
+ <tile id="76">
+  <image width="22" height="14" source="E:/game assets/fields/2 Objects/8 Camp/5.png"/>
+ </tile>
+ <tile id="77">
+  <image width="22" height="13" source="E:/game assets/fields/2 Objects/8 Camp/6.png"/>
+ </tile>
+ <tile id="78">
+  <image width="20" height="36" source="E:/game assets/fields/2 Objects/3 Pointer/1.png"/>
+ </tile>
+ <tile id="79">
+  <image width="16" height="36" source="E:/game assets/fields/2 Objects/3 Pointer/3.png"/>
+ </tile>
+ <tile id="80">
+  <image width="24" height="36" source="E:/game assets/fields/2 Objects/3 Pointer/4.png"/>
+ </tile>
+ <tile id="81">
+  <image width="14" height="35" source="E:/game assets/fields/2 Objects/3 Pointer/2.png"/>
+ </tile>
+ <tile id="82">
+  <image width="24" height="16" source="E:/game assets/fields/2 Objects/3 Pointer/5.png"/>
+ </tile>
+ <tile id="83">
+  <image width="5" height="8" source="E:/game assets/fields/2 Objects/5 Grass/6.png"/>
+ </tile>
+ <tile id="84">
+  <image width="5" height="6" source="E:/game assets/fields/2 Objects/5 Grass/1.png"/>
+ </tile>
+ <tile id="85">
+  <image width="9" height="6" source="E:/game assets/fields/2 Objects/5 Grass/2.png"/>
+ </tile>
+ <tile id="86">
+  <image width="5" height="7" source="E:/game assets/fields/2 Objects/5 Grass/3.png"/>
+ </tile>
+ <tile id="87">
+  <image width="8" height="5" source="E:/game assets/fields/2 Objects/5 Grass/4.png"/>
+ </tile>
+ <tile id="88">
+  <image width="6" height="10" source="E:/game assets/fields/2 Objects/5 Grass/5.png"/>
+ </tile>
+ <tile id="89">
+  <image width="6" height="5" source="E:/game assets/fields/2 Objects/6 Flower/6.png"/>
+ </tile>
+ <tile id="90">
+  <image width="8" height="6" source="E:/game assets/fields/2 Objects/6 Flower/7.png"/>
+ </tile>
+ <tile id="91">
+  <image width="7" height="6" source="E:/game assets/fields/2 Objects/6 Flower/8.png"/>
+ </tile>
+ <tile id="92">
+  <image width="8" height="7" source="E:/game assets/fields/2 Objects/6 Flower/9.png"/>
+ </tile>
+ <tile id="93">
+  <image width="6" height="4" source="E:/game assets/fields/2 Objects/6 Flower/10.png"/>
+ </tile>
+ <tile id="94">
+  <image width="6" height="5" source="E:/game assets/fields/2 Objects/6 Flower/11.png"/>
+ </tile>
+ <tile id="95">
+  <image width="8" height="7" source="E:/game assets/fields/2 Objects/6 Flower/12.png"/>
+ </tile>
+ <tile id="96">
+  <image width="6" height="6" source="E:/game assets/fields/2 Objects/6 Flower/1.png"/>
+ </tile>
+ <tile id="97">
+  <image width="6" height="5" source="E:/game assets/fields/2 Objects/6 Flower/2.png"/>
+ </tile>
+ <tile id="98">
+  <image width="5" height="5" source="E:/game assets/fields/2 Objects/6 Flower/3.png"/>
+ </tile>
+ <tile id="99">
+  <image width="6" height="5" source="E:/game assets/fields/2 Objects/6 Flower/4.png"/>
+ </tile>
+ <tile id="100">
+  <image width="6" height="4" source="E:/game assets/fields/2 Objects/6 Flower/5.png"/>
+ </tile>
+ <tile id="101">
+  <image width="17" height="16" source="E:/game assets/fields/2 Objects/7 Decor/Box1.png"/>
+ </tile>
+ <tile id="102">
+  <image width="18" height="18" source="E:/game assets/fields/2 Objects/7 Decor/Box2.png"/>
+ </tile>
+ <tile id="103">
+  <image width="19" height="18" source="E:/game assets/fields/2 Objects/7 Decor/Box3.png"/>
+ </tile>
+ <tile id="104">
+  <image width="14" height="16" source="E:/game assets/fields/2 Objects/7 Decor/Box4.png"/>
+ </tile>
+ <tile id="105">
+  <image width="10" height="10" source="E:/game assets/fields/2 Objects/7 Decor/Dirt1.png"/>
+ </tile>
+ <tile id="106">
+  <image width="16" height="14" source="E:/game assets/fields/2 Objects/7 Decor/Dirt2.png"/>
+ </tile>
+ <tile id="107">
+  <image width="8" height="9" source="E:/game assets/fields/2 Objects/7 Decor/Dirt3.png"/>
+ </tile>
+ <tile id="108">
+  <image width="9" height="6" source="E:/game assets/fields/2 Objects/7 Decor/Dirt4.png"/>
+ </tile>
+ <tile id="109">
+  <image width="8" height="7" source="E:/game assets/fields/2 Objects/7 Decor/Dirt5.png"/>
+ </tile>
+ <tile id="110">
+  <image width="26" height="23" source="E:/game assets/fields/2 Objects/9 Bush/1.png"/>
+ </tile>
+ <tile id="111">
+  <image width="37" height="26" source="E:/game assets/fields/2 Objects/9 Bush/2.png"/>
+ </tile>
+ <tile id="112">
+  <image width="33" height="22" source="E:/game assets/fields/2 Objects/9 Bush/3.png"/>
+ </tile>
+ <tile id="113">
+  <image width="39" height="25" source="E:/game assets/fields/2 Objects/9 Bush/4.png"/>
+ </tile>
+ <tile id="114">
+  <image width="41" height="25" source="E:/game assets/fields/2 Objects/9 Bush/5.png"/>
+ </tile>
+ <tile id="115">
+  <image width="40" height="26" source="E:/game assets/fields/2 Objects/9 Bush/6.png"/>
+ </tile>
+</tileset>
